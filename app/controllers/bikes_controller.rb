e4da2394a0035c37dca7f368d2555ac1b5ed12ff
Marie Estelle IOrio <marieestelle.iorio@gmail.com>
2019-05-24 11:35:59 +0200

    modifié controller bike

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,36 @@
+class BikesController < ApplicationController
+  def index
+    @bikes = Bike.All
+  end
+
+  def show
+    @bike = Bike.find(:id)
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:name, :brand, :address, :category, :price_per_day, :user_id)
+  end
+end
---
+++ test/controllers/bikes_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class BikesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get bikes_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get bikes_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get bikes_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get bikes_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get bikes_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get bikes_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get bikes_destroy_url
+    assert_response :success
+  end
+
+end
