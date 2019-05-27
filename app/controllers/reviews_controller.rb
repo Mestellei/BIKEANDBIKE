37f181f3d2c60b36f07dce40ec1000d1c5a16fd9
Daniela Azevedo <azevedod@gmail.com>
2019-05-27 12:05:03 +0200

    bookings-index_review-new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,21 @@
 class ReviewsController < ApplicationController
   def new
+    # we need @booking in our `simple_form_for`
+
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    # we need `booking_id` to asssociate review with corresponding booking
+    @review.booking = Booking.find(params[:booking_id])
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
