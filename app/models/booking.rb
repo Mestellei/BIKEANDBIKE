f0768fba536026c08fd2459d55562d66770b5b0a
Daniela Azevedo <azevedod@gmail.com>
2019-05-28 17:39:35 +0200

    controller-model-bookings-index-edit-delete

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :begining_date, :end_date, presence: true
 end
